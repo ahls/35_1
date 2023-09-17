29b6890ce64f318d24d8e0f73b6b7ffdf1f42abf
ahls <ahlon.sin@gmail.com>
2023-09-17 01:51:53 -0700

    added industries

--- data.sql
+++ data.sql
@@ -6,6 +6,8 @@ CREATE DATABASE biztime;
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS companies_industries;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -23,6 +25,16 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries(
+  code text PRIMARY KEY,
+  industry text NOT NULL
+);
+
+CREATE TABLE companies_industries(
+    comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
+    ind_code text NOT NULL REFERENCES industries ON DELETE CASCADE,
+    PRIMARY KEY (comp_code, ind_code)
+    );
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -32,3 +44,14 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries(code, industry)
+  VALUES  ('acc', 'Accounting'),
+          ('sfw', 'Software'),
+          ('hdw', 'Hardware');
+
+INSERT INTO companies_industries(comp_code, ind_code)
+  VALUES ('apple', 'sfw'),
+         ('apple', 'hdw'),
+         ('ibm', 'sfw'),
+         ('ibm', 'hdw');
--- routes/companies.js
+++ routes/companies.js
@@ -11,10 +11,19 @@ router.get('/',async (req,res,next)=>
 })
 router.get('/:code',async (req,res,next)=>
 {
-    const result = await db.query('SELECT code, name, description FROM companies WHERE code = $1', [req.params.code]);
+    const result = await db.query(
+        `SELECT c.code, c.name, c.description, i.industry FROM companies c 
+        LEFT JOIN companies_industries ci ON c.code = ci.comp_code
+        LEFT JOIN industries i on ci.ind_code = i.code 
+        WHERE c.code = $1`
+        , [req.params.code]);
     if (result.rows.length == 0)
         return res.status(404).json({error: 'Not Found'});
-    res.json({company: result.rows[0]});
+
+    const {code,name,description} = result.rows[0];
+    const industries = result.rows.map(r=>r.industry);
+
+    res.json({company: {code,name,description,industries}});
 })
 router.post('/',async (req,res,next)=>
 {  
